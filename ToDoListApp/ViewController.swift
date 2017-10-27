9b27905d8e7c1a4570268e48358fd3f0afe879fe
Rashi Bose <rbose@Rashis-MacBook-Pro.local>
2017-10-27 09:38:40 -0700

    first commit

--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    var toDoList = ["walk the dog", "make the bed", "buy groceries", "mani-pedi", "call mom", "walk the dog", "make the bed", "buy groceries", "mani-pedi", "call mom", "walk the dog", "make the bed", "buy groceries", "mani-pedi", "call mom"]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = toDoList[indexPath.row]
+        cell!.textLabel?.text = text
+        return cell!
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
